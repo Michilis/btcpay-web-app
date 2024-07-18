dec9ea66757aba8a8f8265e6a1d8a6d49425cd4d
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-18 03:01:00 +0200

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  btcpayUserId: {
+    type: String
+  },
+  btcpayStoreId: {
+    type: String
+  },
+  credits: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
