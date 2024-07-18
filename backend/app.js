3a873019bdc3cce766671d1232297c4950c6d117
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-18 03:18:42 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const { sequelize } = require('./models');
 const cors = require('cors');
 
 const app = express();
-
-connectDB();
 
 app.use(cors());
 app.use(express.json({ extended: false }));
@@ -15,4 +13,14 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/subscriptions', require('./routes/subscriptions'));
 app.use('/api/stores', require('./routes/stores'));
 
-module.exports = app;
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    app.listen(5000, () => console.log('Server started on port 5000'));
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+startServer();
