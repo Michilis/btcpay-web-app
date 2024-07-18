67f8a375ce07a584764f11bbe22f2ce6f9da740a
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-18 04:16:15 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const app = require('./app');
-const config = require('config');
 
-const PORT = process.env.BACKEND_PORT || config.get('backendPort');
+const PORT = process.env.BACKEND_PORT || 5005;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
