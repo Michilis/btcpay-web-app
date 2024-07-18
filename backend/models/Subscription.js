fcdae01cf7889aca420d562a534c526641d89ca0
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-18 03:17:33 +0200

    Update Subscription.js

--- backend/models/Subscription.js
+++ backend/models/Subscription.js
@@ -1,22 +1,27 @@
-const mongoose = require('mongoose');
+const { DataTypes } = require('sequelize');
 
-const SubscriptionSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  period: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  expiresAt: {
-    type: Date,
-    required: true
-  }
-});
-
-module.exports = mongoose.model('Subscription', SubscriptionSchema);
+module.exports = (sequelize) => {
+  return sequelize.define('Subscription', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    period: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    expiresAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  });
+};
