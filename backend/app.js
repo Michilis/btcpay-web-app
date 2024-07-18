5ffd8957a409e2853ed3c1126cefe60732dd83eb
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-18 04:15:59 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const config = require('config');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const subscriptionRoutes = require('./routes/subscriptions');
 const storeRoutes = require('./routes/stores');
+
+require('dotenv').config();
 
 const app = express();
 
