37efbb256e31f38bbdcf3cf96d22a1dcb10a6a28
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-18 04:07:46 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const { sequelize } = require('./models'); // Correct path to the models
 const cors = require('cors');
+const config = require('config');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const subscriptionRoutes = require('./routes/subscriptions');
+const storeRoutes = require('./routes/stores');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('API Running'));
-
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/subscriptions', require('./routes/subscriptions'));
-app.use('/api/stores', require('./routes/stores'));
+app.use('/api/auth', authRoutes);
+app.use('/api/subscriptions', subscriptionRoutes);
+app.use('/api/stores', storeRoutes);
 
 module.exports = app;
