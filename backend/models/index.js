dc27c3486e4b9a6c42c520ccd6e189e37e6d2e9e
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-18 03:22:06 +0200

    Create index.js

---
+++ backend/models/index.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+const config = require('config');
+
+const sequelize = new Sequelize(config.get('postgresURI'), {
+  dialect: 'postgres',
+  logging: false
+});
+
+const User = require('./User')(sequelize);
+const Subscription = require('./Subscription')(sequelize);
+const Store = require('./Store')(sequelize);
+
+User.hasMany(Subscription, { foreignKey: 'userId' });
+Subscription.belongsTo(User, { foreignKey: 'userId' });
+
+User.hasMany(Store, { foreignKey: 'userId' });
+Store.belongsTo(User, { foreignKey: 'userId' });
+
+sequelize.sync();
+
+module.exports = {
+  sequelize,
+  User,
+  Subscription,
+  Store
+};
