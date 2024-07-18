a257a00131cfc5c407f0af5587b36be346f7aeb0
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-18 03:17:21 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,25 +1,30 @@
-const mongoose = require('mongoose');
+const { DataTypes } = require('sequelize');
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  btcpayUserId: {
-    type: String
-  },
-  btcpayStoreId: {
-    type: String
-  },
-  credits: {
-    type: Number,
-    default: 0
-  }
-});
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = (sequelize) => {
+  return sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    btcpayUserId: {
+      type: DataTypes.STRING
+    },
+    btcpayStoreId: {
+      type: DataTypes.STRING
+    },
+    credits: {
+      type: DataTypes.FLOAT,
+      defaultValue: 0
+    }
+  });
+};
